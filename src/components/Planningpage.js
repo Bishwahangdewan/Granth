64c60bfa4564a50dcb71a2be9a10613a3bdacb42
Keshav Garg <gargk753@gmail.com>
2022-01-28 14:45:18 +0530

    commited28

--- src/components/Planningpage.js
+++ src/components/Planningpage.js
@@ -32,7 +32,7 @@ function Planningpage(props) {
           <img
             src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             height="500px"
-            width="800px"
+            width="100%"
             alt=""
           />
         </div>
